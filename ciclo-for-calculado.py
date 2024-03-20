ae4f2ae0022d46136cbb9b2204d0d9b9582815d3
Jartaviap <162387942+Jartaviap@users.noreply.github.com>
2024-03-20 03:06:14 +0000

    Cambios leccion 4

---
+++ ciclo-for-calculado.py
@@ -0,0 +1,8 @@
+# lista = list(range(5))
+# print(lista)
+
+lista_etapas_lavado = ["Llenado", "Lavado", "Enguaje", "Drenado", "Listo"]
+
+for i in range(len(lista_etapas_lavado)):
+    print(i)
+    print(lista_etapas_lavado[i])
---
+++ ciclo-while-calculado.py
@@ -0,0 +1,19 @@
+arreglo = [1, 2, 3, 4, 5]
+
+print(arreglo[3])
+
+
+arreglo_cadena = ["Kevin", "Z", "Y", "X", "W", "V"]
+
+indice = 0
+
+tamaño_arreglo = len(arreglo_cadena)
+
+while indice <= tamaño_arreglo - 1:
+    print("Valor de índice: "+ str(indice))
+    print("Valor de elemento: "+ str(arreglo_cadena[indice]))
+    indice = indice + 1
+
+print("Seguimos el flujo")
+print("Fin")
+
